d8db53ae9e3ffc068bdfcc42b22ee0f4698b4e9d
Nicklackis <96375900+nicklakiss@users.noreply.github.com>
2023-04-05 17:37:11 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+static IList<string> FizzBuzz(int number)
+{
+    if (number / 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (number / 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+} 
